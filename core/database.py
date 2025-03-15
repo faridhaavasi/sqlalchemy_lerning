5c52edb1ee768f12670a4be0f1e032eea43e6ab6
faridhaavasi <faridhavasi777@gmail.com>
2025-03-15 16:08:56 +0330

    Add User model to database schema

--- core/database.py
+++ core/database.py
@@ -1,5 +1,4 @@
-from sqlalchemy import create_engine
-
+from sqlalchemy import create_engine, Column, String, Integer
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
@@ -9,5 +8,14 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    lastname = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
 
 Base.metadata.create_all(bind=engine)
